895c37b1e56cbefbcc0cfa270617acc243665227
bzsol <bereczzsolt2000@gmail.com>
2021-02-20 17:18:00 +0100

    5. change

--- Projekt.c
+++ Projekt.c
@@ -7,20 +7,20 @@ int main(int argc, char const *argv[])
 {
 
 	if(argc == 1){
-	
+
 	//This is the buffer and allocated size of the string
 	char* filename = malloc(256*sizeof(char));
-	
+
 	// The input
 	printf("Please enter the filename: ");
 	scanf("%s",filename);
-	printf("\n");
-	
+
 	// We realloc the memory for the better because we don't need 256 char
 	filename = realloc(filename,sizeof(filename)/sizeof(filename[0]));
 	puts(filename);
+    printf("strlen: %d\n",strlen(filename));
+    printf("char* size: %d",sizeof(filename)/sizeof(filename[0]));
 
-	
 	// Free the allocated memory
 	free(filename);
 
@@ -35,7 +35,7 @@ int main(int argc, char const *argv[])
 		printf("########################\n");
 		printf("#    Date: 11.02.2021  #\n");
 		printf("########################\n");
-		
+
 	}
 	else if(argc == 2 && (strcmp("--help",argv[1]) == 0)){
 		printf("Functions and Commands:\n");
