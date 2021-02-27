43feda89030c3779a544d8a57d9b5b077848a799
bzsol <bereczzsolt2000@gmail.com>
2021-02-27 02:39:57 -0800

    First pixel okay

--- bitwise.c
+++ bitwise.c
@@ -2,26 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+char pixel;
 char* str;
-char* pixel;
 char* TestArray(int *NumCh){
+    char changer;
     const char alap[3] = "abc";
-    str = (char*)malloc(256*sizeof(char));
-    *NumCh = strlen(alap);
+    *NumCh = sizeof(alap)/sizeof(alap[0]);
+
+    // 4 alapeset
+    for(int i = 5;0<=i;i--){
+        int x = rand()%(1-0+1)+0;
+            //printf("%d",x);
+            if(x == 1){
+                pixel ^= (1 << i);
+            }
+    }
+    pixel <<= 2; // 3 alapeset
+
+    changer = (changer|alap[0]);
+    changer >>= 6;
+    pixel = (pixel|changer);
+    str = (char*)malloc(100*sizeof(char));
     sprintf(str,"%p",&pixel);
-    return str;
+ return str;
+}
+
+
+
+void charbit(char val) {
+  for (int i=7; 0<=i;i--) {
+    printf("%c", (val & (1 << i)) ? '1' : '0');
+  }
+  printf("\n");
 }
 
 
 int main(int argc, char const *argv[])
 {
-
-    pixel = (char*)malloc(9*sizeof(char));
     int num;
-    char* memcim = TestArray(&num);
-    printf("%s\n", memcim);
-    free(pixel);
+    //charbit(pixel);
+    char* alap = TestArray(&num);
+    printf("pixel: ");
+    charbit(pixel);
+    printf("\n");
+    printf("titkos hossz: %d\n",num);
+    printf("memcim: %s\n",str);
     free(str);
-
     return 0;
 }
